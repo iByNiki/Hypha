3bb97cae5001ee5503ee40402e04fc11686658fa
Niki <39004156+iByNiki@users.noreply.github.com>
2023-11-23 19:09:13 +0100

    cli.py - added simple argparse

--- cli.py
+++ cli.py
@@ -1,6 +1,29 @@
 import sys
 import hypha.core.builder as builder
+import argparse
 
 # Logic with system arguments
 
-builder.build()
+def build():
+    builder.build()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="Hypha",
+        description="The Javascript Framework for PHP"
+    )
+
+    parser.add_argument("cmd", type=str, help="build / dev")
+    parser.add_argument("-p", "--port")
+
+    args = parser.parse_args()
+
+    if (args.cmd == "build"):
+        build()
+    elif (args.cmd == "dev"):
+        # Start dev server with automatic building and reloading
+        pass
+
+if (__name__ == "__main__"):
+    main()
