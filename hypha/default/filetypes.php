09cab9d5dc7cc6252ae639e92ca5e9840f040f27
Niki <39004156+iByNiki@users.noreply.github.com>
2023-11-25 14:44:25 +0100

    Added dev server to the CLI

---
+++ hypha/default/filetypes.php
@@ -0,0 +1,16 @@
+<?php
+
+global $fileTypes;
+$fileTypes = [
+
+    "css" => "text/css",
+    "js" => "application/javascript",
+    "xml" => "application/xml",
+    "zip" => "application/zip",
+    "ogg" => "application/ogg",
+    "pdf" => "application/pdf",
+    "json" => "application/json"
+
+];
+
+?>
--- hypha/default/router.php
+++ hypha/default/router.php
@@ -1,5 +1,7 @@
 <?php
 
+include("filetypes.php");
+
 class Router {
 
     private $urls = array();
@@ -14,6 +16,8 @@ class Router {
 
     public function run() {
 
+        global $fileTypes;
+
         $requestUrl = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
 	    $requestUrl = strtok($requestUrl, "?");
 
@@ -26,7 +30,24 @@ class Router {
             }
         }
 
-        if (!$found && file_exists("public" . $requestUrl)) {
+        if (!$found && file_exists(__DIR__ . "/public" . $requestUrl)) {
+            $publicPath = __DIR__ . "/public" . $requestUrl;
+            $extension = pathinfo($publicPath)["extension"];
+
+            if (array_key_exists($extension, $fileTypes)) { // Check if in file types array
+                $contentType = $fileTypes[$extension];
+            } else {
+                if (function_exists("finfo_file")) {
+                    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                    $contentType = finfo_file($finfo, $publicPath);
+                    finfo_close($finfo);
+                } else { // For old php versions
+                    $contentType = mime_content_type($publicPath);
+                }
+            }
+
+            header("Content-type: " . $contentType);
+            include($publicPath);
             exit();
         }
 
@@ -35,7 +56,8 @@ class Router {
             if (in_array("/404", $this->urls)) {
                 $this->route($this->paths[array_search("404", $this->urls)]);
             } else {
-                echo "Page not found";
+                echo "<h1>404</h1>";
+                echo "</p>Page not found<p>";
                 exit();
             }
         }
