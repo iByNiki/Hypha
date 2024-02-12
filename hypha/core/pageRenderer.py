2ba6ae95b2ea406d69efc673d029e37819a44a76
Niki <39004156+zzniki@users.noreply.github.com>
2024-02-12 11:49:01 +0100

    Performance improvements + fixes
    Boosted page rendering performance by 180%:
        - Added compiled script code caching
    
    Fixed "elemType" bug in head config
    Fixed requirements.txt
    Fixed builder.py path errors

--- hypha/core/pageRenderer.py
+++ hypha/core/pageRenderer.py
@@ -40,7 +40,7 @@ class PageRenderer(object):
                     elemObj.innerHTML = elem["inner"]
 
                 for key in elem:
-                    if (key.lower() != "elemtype" and key.lower() != "inner"):
+                    if (key.lower() != "elemType" and key.lower() != "inner"):
                         elemObj.attribs.append(HTMLAttribute(key.lower(), elem[key]))
 
                 finalHead.addChild(elemObj)
@@ -129,8 +129,7 @@ class PageRenderer(object):
         # Process
         for bundledScript in bundled:
 
-            for dep in bundledScript.getLangDeps():
-                if (dep not in jsLangDeps): jsLangDeps.append(dep)
+            jsLangDeps += bundledScript.getLangDeps()
 
             if (bundledScript.defer):
                 deferredJs += bundledScript.getCompiledCode()
@@ -138,12 +137,12 @@ class PageRenderer(object):
                 finalJs += bundledScript.getCompiledCode()
 
         for unbundledScript in notBundled:
-            for dep in unbundledScript.getLangDeps():
-                if (dep not in jsLangDeps): jsLangDeps.append(dep)
+            jsLangDeps += unbundledScript.getLangDeps()
 
         finalHead.addChild(HTMLElement("script", attribs=[HTMLAttribute("src", "/hjs/hypha.js")]))
 
         # Dependencies
+        jsLangDeps = list(set(jsLangDeps)) # Removes duplicates
         for dep in jsLangDeps:
             finalHead.addChild(HTMLElement("script", attribs=[HTMLAttribute("src", "/hjs/" + dep)]))
 
